5aaf35078c18fc0fb460c95a6fc7558ce66b13b8
Juh Ohbayashi <jun.ohbayashi@arsaga.jp>
2022-12-15 11:49:27 +0900

    @storybook/jest

--- src/components/test.stories.tsx
+++ src/components/test.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 import { Test } from "./test";
+import { userEvent, within } from "@storybook/testing-library";
 
 export default {
   component: Test,
